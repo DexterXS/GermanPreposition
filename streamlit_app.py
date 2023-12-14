205e8420bbb305ac6f6a73eeb683c68dd7123bc0
Vladyslav Handzii <root.user76@gmail.com>
2023-12-14 14:39:57 +0100

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,28 +12,27 @@ verbs_info = {
 }
 
 def get_random_verb():
-    """Функція для вибору випадкового дієслова."""
     verb, info = random.choice(list(verbs_info.items()))
     return verb, info
 
 # Створення веб-додатку з використанням Streamlit
 st.title("Вивчення німецьких дієслів з прийменниками")
 
-if st.button("Отримати дієслово"):
+if 'current_verb' not in st.session_state or st.button("Отримати нове дієслово"):
     verb, info = get_random_verb()
     st.session_state['current_verb'] = verb
     st.session_state['current_info'] = info
-    st.write(f"Дієслово: {verb} - {info['translation']}")
 
-if 'current_verb' in st.session_state:
-    verb = st.session_state['current_verb']
-    info = st.session_state['current_info']
+verb = st.session_state['current_verb']
+info = st.session_state['current_info']
 
-    user_case = st.selectbox("Виберіть відмінок", ["Dativ", "Akkusativ"])
-    user_preposition = st.text_input("Введіть прийменник")
+st.write(f"Дієслово: {verb} - {info['translation']}")
 
-    if st.button("Перевірити відповідь"):
-        if user_case == info['case'] and user_preposition == info['preposition']:
-            st.success("Вірно!")
-        else:
-            st.error(f"Неправильно. Правильна відповідь: {verb} ({info['case']} {info['preposition']})")
+user_case = st.selectbox("Виберіть відмінок", ["Dativ", "Akkusativ"])
+user_preposition = st.text_input("Введіть прийменник")
+
+if st.button("Перевірити відповідь"):
+    if user_case == info['case'] and user_preposition == info['preposition']:
+        st.success("Вірно!")
+    else:
+        st.error(f"Неправильно. Правильна відповідь: {verb} ({info['case']} {info['preposition']})")
