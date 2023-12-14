7710422a85394de2701771a10366767c736a868e
Vladyslav Handzii <root.user76@gmail.com>
2023-12-14 16:43:36 +0100

    reload

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,29 +2,30 @@ import streamlit as st
 import json
 import random
 
-# Завантаження даних з JSON файлів
+# Loading data from JSON files
 def load_data(akkusativ_file, dativ_file):
     with open(akkusativ_file, 'r', encoding='utf-8') as file:
-        akkusativ_data = json.load(file)["Verben mit Präposition + Akkusativ"]
+        akkusativ_data = json.load(file)
     with open(dativ_file, 'r', encoding='utf-8') as file:
-        dativ_data = json.load(file)["Verben mit Präposition + Dativ"]
-    # Об'єднуємо словники
+        dativ_data = json.load(file)
+
+    # Combine dictionaries
     return {**akkusativ_data, **dativ_data}
 
-# Отримання випадкового дієслова зі списку
+# Getting a random verb from the list
 def get_random_verb(verbs_dict):
-    verbs = list(verbs_dict)
+    verbs = list(verbs_dict.keys())
     verb = random.choice(verbs)
     return verb, verbs_dict[verb]
 
-# Шляхи до файлів JSON
+# Paths to JSON files
 akkusativ_file_path = 'akkusativ_verbs_full_translations.json'
 dativ_file_path = 'dativ_verbs_with_prepositions.json'
 
-# Завантажуємо дані з файлів
+# Loading data from files
 verbs_data = load_data(akkusativ_file_path, dativ_file_path)
 
-# Створення веб-додатку з використанням Streamlit
+# Streamlit web app creation
 st.title("Вивчення німецьких дієслів з прийменниками")
 
 if st.button("Отримати нове дієслово"):
